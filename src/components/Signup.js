49f3d4807880ca823064bb51b022f25ed5f2f2d7
ShivaRK <shivarkcodes@gmail.com>
2023-01-22 11:01:53 +0530

    Made signup form required

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -50,20 +50,20 @@ const Signup = (props) => {
             <form onSubmit={handleSubmit}>
                 <div className="mb-3">
                     <label htmlFor="name" className="form-label">Name</label>
-                    <input type="name" className="form-control" name='name' id="name" onChange={onchange} aria-describedby="emailHelp"/>
+                    <input type="name" required className="form-control" name='name' id="name" onChange={onchange} aria-describedby="emailHelp"/>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="email" className="form-label">Email address</label>
-                    <input type="email" className="form-control" name='email' id="email" onChange={onchange} aria-describedby="emailHelp"/>
+                    <input type="email" required className="form-control" name='email' id="email" onChange={onchange} aria-describedby="emailHelp"/>
                         <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="password" className="form-label">Password</label>
-                    <input type="password" className="form-control" onChange={onchange} name='password' id="password"/>
+                    <input type="password" required className="form-control" onChange={onchange} name='password' id="password"/>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="cpassword" className="form-label">Confirm Password</label>
-                    <input type="password" className="form-control" onChange={onchange} name='cpassword' id="cpassword"/>
+                    <input type="password" required className="form-control" onChange={onchange} name='cpassword' id="cpassword"/>
                 </div>
                 <button type="submit" className="btn btn-primary">{submit}</button>
             </form>
